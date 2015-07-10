9b20424e70a1bbae5cc86b34de9a769e002605d3
Nachiket Kakatkar <nachiketkakatkar@gmail.com>
2015-07-10 09:16:31 +0530

    Get to work most of the functionality
    1. Updation of when non-mine square detected
    2. Add safe mines color

--- js/MineField.jsx
+++ js/MineField.jsx
@@ -1,37 +1,70 @@
 var MineField = React.createClass({
   getInitialState: function(){
-    FieldGenerator.generate(10, 20);
     return {
-      mineField: FieldGenerator.field
+      mineField: new FieldGenerator(10, 20)
     };
   },
   render: function() {
-    var p = this.props, i, j, mine, l = this.state.mineField.length;
+    var p = this.props, i, j, mine, l = this.state.mineField.cells.length;
     var self = this;
     return (
       <table id="mineField">
+        <thead>
+          <tr><button onClick={this._handleResetClick}>Reset</button></tr>
+          <tr><button onClick={this._handleRevealClick}>Reveal</button></tr>
+        </thead>
         <tbody>
-    {this.state.mineField.map(function(row, i) {
+    {this.state.mineField.cells.map(function(row, i) {
         return (
           <tr>
           {row.map(function(mine, j) {
-            return self._renderMine(mine);
+            return self._renderMine(mine, i, j);
           })}
       </tr>
     );
-  })}
+  })
+  }
         </tbody>
       </table>
     );
   },
-  _renderMine: function(mine) {
-    if(mine.isMine) {
-      return <td><button onClick={this._handleMineClick.bind(this, mine)}>M</button></td>;
+  _renderMine: function(mine, row, col) {
+    var text, disabled = '', className = '';
+    if(mine.detected){
+      disabled = 'disabled';
+      if(mine.isMine){
+        if(mine.blown){
+          className = 'blown';
+        } else {
+          className = 'safe';
+        }
+        text = 'M';
+      } else {
+        text = mine.nMines;
+      }
     }
-    return <td><button onClick={this._handleMineClick.bind(this, mine)}>{mine.nMines}</button></td>;
+    return <td><button className={className} disabled={disabled} onClick={this._handleMineClick.bind(this, mine, row, col)}>{text}</button></td>;
   },
-  _handleMineClick: function(mine, ev) {
+  _handleMineClick: function(mine, row, col, ev) {
      console.log('Clicked mine ! ', mine);
+     if(!ev.ctrlKey && mine.isMine) {
+       this.state.mineField.blowUp();
+     } else {
+       mine.reveal();
+       this.state.mineField.revealVicinity(row, col);
+     }
+     //TODO : This does not make sense :)
+     this.setState(this.state.mineField);
+   },
+   _handleResetClick: function(ev) {
+     this.state.mineField.reset();
+     //TODO : This does not make sense :)
+     this.setState(this.state.mineField);
+   },
+   _handleRevealClick: function(ev) {
+     this.state.mineField.reveal();
+     //TODO : This does not make sense :)
+     this.setState(this.state.mineField);
    }
 });
 
--- js/generator.js
+++ js/generator.js
@@ -2,67 +2,121 @@ var Cell = function(nMines, blown,isMine) {
   this.nMines = nMines;
   this.blown = blown;
   this.isMine = isMine;
+  this.detected = false;
 
   this.setIsMine = function (isMine) {
     this.isMine = isMine;
   };
   this.explode = function() {
-    this.blown = true;
+    if(this.isMine && !this.detected){
+      this.blown = true;
+    }
   };
   this.addMine = function() {
     this.nMines++;
-  }
+  };
+  this.reveal = function() {
+    this.detected = true;
+  };
 };
 
-var FieldGenerator = (function(){
-  var field = [];
-
+var FieldGenerator = function(l, n){
+  this.cells = [];
+  this.nMines = n;
+  this.gridSize = l;
   /*
-  Randomly lay out 'n' number of mines in l X l grid
-  * */
-  function generate (l, n) {
+   Randomly lay out 'n' number of mines in l X l grid
+   * */
+  this.generate = function() {
     var i, j, row, col;
-    this.field = [];
-    this.nMines = n;
-    this.gridSize = l;
+    this.cells = [];
     //empty cells
     for(i = 0; i < l; i++) {
       row = [];
       for(j = 0; j < l; j++) {
         row.push(new Cell(0, false, false));
       }
-      this.field.push(row);
+      this.cells.push(row);
     }
     this.layoutMines();
-  }
+  };
 
-  function layoutMines() {
+  this.layoutMines = function() {
     var i, j;
     for(i = 0; i < this.nMines; i++) {
       do {
         row = Math.ceil((Math.random() * 1000)) % this.gridSize;
         col = Math.ceil((Math.random() * 1000)) % this.gridSize;
-      } while(this.field[row][col].isMine);
+      } while(this.cells[row][col].isMine);
 
-      this.field[row][col].setIsMine(true);
+      this.cells[row][col].setIsMine(true);
       this.updateVicinity(row, col);
     };
-  }
+  };
 
-  function updateVicinity(row, col) {
-    var cell = this.field[row][col], l = this.field.length, i, j;
+  this.updateVicinity = function(row, col) {
+    var cell = this.cells[row][col], l = this.cells.length, i, j;
     for(i = row - 1; i <= row + 1; i++) {
       for(j = col - 1; j <= col + 1; j++) {
-        if(!(i === row && j === col) && this.field[i] && this.field[i][j] && !this.field[i][j].isMine) {
-          this.field[i][j].addMine();
+        if(!(i === row && j === col) && this.cells[i] && this.cells[i][j] && !this.cells[i][j].isMine) {
+          this.cells[i][j].addMine();
         }
       }
     }
-  }
-  return {
-    field: field,
-    generate: generate,
-    updateVicinity: updateVicinity,
-    layoutMines: layoutMines
   };
-})();
+
+  this.checkRevealVicinity = function(row, col) {
+    var cell = this.cells[row][col], i, j, minesInVicinity = 0;
+    if(cell.nMines !== 0) {
+      for(i = row - 1; i <= row + 1; i++) {
+        for(j = col - 1; j <= col + 1; j++) {
+          if(!(i === row && j === col) && this.cells[i] && this.cells[i][j] && this.cells[i][j].isMine && this.cells[i][j].detected) {
+            minesInVicinity++;
+          }
+        }
+      }
+      if(minesInVicinity === cell.nMines) {
+        this.revealVicinity(row, col);
+      }
+    } else {
+      this.revealVicinity(row, col);
+    }
+  };
+
+  this.revealVicinity = function(row, col) {
+    var cell = this.cells[row][col], i, j, minesInVicinity = 0;
+    if(minesInVicinity === cell.nMines) {
+      for(i = row - 1; i <= row + 1; i++) {
+        for(j = col - 1; j <= col + 1; j++) {
+          if(!(i === row && j === col) && this.cells[i] && this.cells[i][j]) {
+            this.cells[i][j].reveal();
+            //this.checkRevealVicinity(row, col);
+          }
+        }
+      }
+    }
+  };
+
+  this.blowUp = function(){
+    this.cells.forEach(function(row){
+      row.forEach(function(cell){
+        cell.explode();
+        cell.reveal();
+      });
+    });
+  };
+
+  this.reset = function() {
+    this.generate();
+  };
+
+  this.reveal = function(){
+    this.cells.forEach(function(row){
+      row.forEach(function(cell){
+        cell.reveal();
+      });
+    });
+  };
+
+  this.generate();
+};
